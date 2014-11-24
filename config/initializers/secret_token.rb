797d2f1b96758fce5277ff5c63570f43c9180742
Masashi Takata <takatashiki@gmail.com>
2014-11-24 21:26:09 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentalpeace::Application.config.secret_key_base = '873b2475ae8f67fcabd279158b705a842b5092e07e10722830b57d736b00d89a6d7a9e4f0831cee2db198bfc2ae9c41f5b983d5d01a65fd6e1406aa7e3050c45'
